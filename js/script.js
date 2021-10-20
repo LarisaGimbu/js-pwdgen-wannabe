c580a2670087132a960f3ac29ce302ce25ca5325
LarisaGimbu <gimbularisa@gmail.com>
2021-10-20 17:06:06 +0200

    punto e virgola

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const firstname = prompt("To find out your password please enter your firstname")
-const lastname = prompt("To find out your password please enter your lastname")
-const favouriteColor = prompt("To find out your password please enter your favourite color")
+const firstname = prompt("To find out your password please enter your firstname");
+const lastname = prompt("To find out your password please enter your lastname");
+const favouriteColor = prompt("To find out your password please enter your favourite color");
 
-// console.log (firstname)
-// console.log (lastname)
-// console.log (favouriteColor)
+// console.log (firstname);
+// console.log (lastname);
+// console.log (favouriteColor);
 
 const textToPrint = `
   <strong> Your new password is:</strong> ${firstname}${lastname}${favouriteColor}21
-`
-// console.log(textToPrint)
+`;
+// console.log(textToPrint);
 
-document.getElementById("password-generator").innerHTML = textToPrint
+document.getElementById("password-generator").innerHTML = textToPrint;
