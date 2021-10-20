61775d7f501e8fc5b547c6656f4e96758dfe187c
LarisaGimbu <gimbularisa@gmail.com>
2021-10-20 14:52:18 +0200

    variabili

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const firstname = prompt("Inserisci il tuo nome")
+const lastname = prompt("Inserisci il tuo cognome")
+const favouriteColor = prompt("Inserisci il tuo colore preferito")
+
+// console.log (firstname)
+// console.log (lastname)
+// console.log (favouriteColor)
+
