30099cdcc19b65d756b2bd113736192c223e57bc
LarisaGimbu <gimbularisa@gmail.com>
2021-10-20 15:58:54 +0200

    bootstrap

--- js/script.js
+++ js/script.js
@@ -10,4 +10,5 @@ const textToPrint = `
   <strong> Your new password is:</strong> ${firstname}${lastname}${favouriteColor}21
 `
 // console.log(textToPrint)
+
 document.getElementById("password-generator").innerHTML = textToPrint
